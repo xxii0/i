85d59f3c1934c4a03b4dd728c9cbe0df89c30f34
xxii0 <124769572+xxii0@users.noreply.github.com>
2023-02-27 13:48:24 +0800

    Update i.js

--- i.js
+++ i.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://js.users.51.la/21556063.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 const div = document.createElement('div');
 const a = document.createElement('a');
 a.href = 'https://mst.vip.com/iYcEvwVGaLI-Bf_5PtVKAQ.php?wq=1&wapid=mst_100041464&_src=mst&extra_banner=115041464&nova=1&nova_platform=1&mst_page_type=guide';
@@ -13,3 +8,9 @@ img.width = '10px';
 a.appendChild(img);
 div.appendChild(a);
 document.body.appendChild(div);
+
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://js.users.51.la/21556063.js';
+document.getElementsByTagName('head')[0].appendChild(script);
